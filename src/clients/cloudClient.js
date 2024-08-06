38d044582b613a567ddb113d1564549b32743638
NinoLoncar <nino.loncar16@gmail.com>
2024-08-06 18:14:17 +0200

    Add postDevices to cloudClient

--- src/clients/cloudClient.js
+++ src/clients/cloudClient.js
@@ -6,10 +6,24 @@ class CloudClient {
 		let resposne = await fetch(baseUrl + "/devices");
 		if (resposne.status == 200) {
 			let devices = await resposne.text();
+			console.log(devices);
 			return JSON.parse(devices);
 		} else {
 			return null;
 		}
+	};
+
+	postDevices = async function (device, apiKey) {
+		let parameters = {
+			headers: {
+				"Content-Type": "application/json",
+				"Api-Key": apiKey,
+			},
+			method: "POST",
+			body: JSON.stringify(device),
+		};
+		let resposne = await fetch(baseUrl + "/devices", parameters);
+		return resposne.status;
 	};
 }
 module.exports = CloudClient;
