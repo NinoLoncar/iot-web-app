bd65277d7ad6cc44d840af9235b4575f1ada80ea
NinoLoncar <nino.loncar16@gmail.com>
2024-08-08 22:46:30 +0200

    Add serving of device details page

--- src/controllers/pageController.js
+++ src/controllers/pageController.js
@@ -1,5 +1,8 @@
 const path = __dirname;
 const ds = require("fs/promises");
+const CloudClient = require("../clients/cloudClient");
+
+const cloudClient = new CloudClient();
 
 class PageController {
 	getDevicesPage = async function (req, res) {
@@ -9,6 +12,18 @@ class PageController {
 
 	getNewDevicePage = async function (req, res) {
 		let html = await loadHtml("newDevice");
+		res.send(html);
+	};
+
+	getDeviceDetailsPage = async function (req, res) {
+		let device = await cloudClient.getDevice(req.params.androidId);
+		let html;
+		console.log(device);
+		if (device == null || JSON.stringify(device) == "{}") {
+			html = await loadHtml("deviceNotRegistered");
+		} else {
+			html = await loadHtml("deviceDetails");
+		}
 		res.send(html);
 	};
 }
--- src/server.mjs
+++ src/server.mjs
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv/config";
 import PageController from "./controllers/pageController.js";
 import DeviceApiController from "./controllers/deviceApiController.js";
+import SensorDataApiController from "./controllers/sensorDataApiController.js";
 
 const server = express();
 const port = process.env.PORT || 3000;
 const pageController = new PageController();
 const deviceApiController = new DeviceApiController();
+const sensorDataApiController = new SensorDataApiController();
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
@@ -28,9 +30,15 @@ server.get(
 );
 
 server.get("/new-device", pageController.getNewDevicePage.bind(pageController));
+server.get(
+	"/registered-devices/:androidId",
+	pageController.getDeviceDetailsPage
+);
 
 server.get("/devices", deviceApiController.getDevices);
 server.post("/devices", deviceApiController.postDevices);
+
+server.get("/sensor-data", sensorDataApiController.getSensorData);
 
 server.listen(port, async () => {
 	console.log(`Server pokrenut na portu: ${port}`);
